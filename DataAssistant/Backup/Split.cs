e7353b7073eb09cacd79c2ae0982d52723591b5e
rd124p7 <rd124p7@yahoo.com>
2017-07-23 21:56:08 -0400

    Added Split class

---
+++ DataAssistant/Backup/Split.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAssistant.Backup
+{
+    class Split
+    {
+        private string hdd_name = "";
+
+        public Split(string hdd_name)
+        {
+            this.hdd_name = hdd_name;
+        }
+    }
+}
