a8ca39f62ee85aee24a2cd143a61a16ac834621d
rd124p7 <rd124p7@yahoo.com>
2017-07-26 19:58:55 -0400

    Updated the split class again

--- DataAssistant/Backup/Split.cs
+++ DataAssistant/Backup/Split.cs
@@ -1,4 +1,38 @@
-﻿using System;
+﻿/*
+MIT License
+
+Copyright (c) 2017 rd124p7
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
+    Class Name: Split
+    Constructor: Split
+    Inherits: None
+
+    Description: 
+        Perform operations to split the contents of a hard drive and return the results,
+        operations to make sure the returned lists are around the same length
+**/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,24 +44,54 @@ namespace DataAssistant.Backup
     class Split
     {
         private string hdd_name = "";
+        private string[] dirContents = { };
+        private string[] fileContents = { };
 
+        /**
+            Constructor: Split
+            Description:
+                When a new Split instance is created pass in the letter of the hard drive,
+                then set instance variables dirContents and fileContents
+
+            Params: 
+                hdd_name -> string
+            Returns: None
+        **/
         public Split(string hdd_name)
         {
             this.hdd_name = hdd_name;
+            dirContents = Directory.GetDirectories(this.hdd_name);
+            fileContents = Directory.GetFiles(this.hdd_name);
         }
 
-        /**
-            Method Name: splListsFolders
-            Description:
-                Split up the folders and files in the root directory, send them on to there seperate arrays 
-
-            Params: None
-            Returns: folderList -> List<List<string>>
-        **/
-        public List<string[]> splListsFoldersFiles()
+        public List<string> getFirstSectionFolder()
         {
-            return new List<string[]> { Directory.GetDirectories(this.hdd_name), Directory.GetFiles(this.hdd_name) };
+            List<string> temp = new List<string>();
+
+            for (int i = 0; i < dirContents.Length / 2; i++)
+                temp.Add(dirContents[i]);
+
+            return temp;
         }
 
+        public List<string> getLastSectionFolder()
+        {
+            List<string> temp = new List<string>();
+
+            for (int i = dirContents.Length / 2; i < dirContents.Length; i++)
+                temp.Add(dirContents[i]);
+
+            return temp;
+        }
+
+        public List<string> getFileSection()
+        {
+            List<string> temp = new List<string>();
+
+            for (int i = 0; i < fileContents.Length; i++)
+                temp.Add(fileContents[i]);
+
+            return temp;
+        }
     }
 }
--- DataAssistant/Form1.cs
+++ DataAssistant/Form1.cs
@@ -58,7 +58,8 @@ namespace DataAssistant
 
 
             Backup.Split spl = new Backup.Split(dev.GetHDDLetters()[0]);
-            spl.splListsFoldersFiles();
+            for (int i = 0; i < spl.getFirstSectionFolder().Count; i++) Console.WriteLine(spl.getFirstSectionFolder()[i]);
+            for (int i = 0; i < spl.getLastSectionFolder().Count; i++) Console.WriteLine(spl.getLastSectionFolder()[i]);
         }
     }
 }
